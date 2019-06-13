d988def38b8d8aa051b1fbc2e3181553dc2bca09
Rubén Segura <rsegrom@gmail.com>
2019-06-13 18:35:53 +0200

    Estructura página de inicio

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<!-- Carga el header -->
 <?php get_header() ?>
-<p>Prueba de contenido</p>
+<!-- título -->
+<div class="row"> 
+    <h1>Listado de post publicados</h1>
+</div>
+<!-- contenido -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="row">
+        <div class="col-sm-8">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h5>
+                <p class="card-text">
+                    <?php the_excerpt() ?>
+                </p>
+                <a href="<?php the_permalink(); ?>" class="btn btn-primary">Seguir leyendo</a>
+            </div>
+            </div>
+        </div>
+    </div>
+<?php endwhile; else : ?>
+        <p><?php esc_html_e( 'No se ha encontrado ningún resultado.', 'rdv' ); ?></p>
+<?php endif; ?>
+<!-- Carga el footer -->
 <?php get_footer() ?>
