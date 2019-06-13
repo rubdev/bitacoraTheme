ebc041ed35c7c8ef13216b75f40c0e018d37e42c
Rubén Segura <rsegrom@gmail.com>
2019-06-13 13:46:15 +0200

    Añadido archivo header

---
+++ header.php
@@ -0,0 +1,10 @@
+<?php require_once ('functions.php'); ?>
+<!doctype html>
+<html <?php language_attributes() ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ) ?>"/>
+    <link rel="pinback" href="<?php bloginfo( 'pinback_url' ) ?>"/>
+    <?php wp_head() ?>
+</head>
+<body <?php body_class() ?>>
+    <div class="container-fluid">
