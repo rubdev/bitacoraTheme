74ab483e2c6eeb4b4531ea9e08d24fd1efadc6ac
Rubén Segura <rsegrom@gmail.com>
2019-06-13 12:50:16 +0200

    Agregada screenshot, clases navwalker y dir img con logo

---
+++ includes/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-md navbar-light bg-faded justify-content-end">
+  <div class="container">
+    <a class="navbar-brand" href="<?php get_home_url( ) ?>">
+    <?php
+        if ( function_exists( 'the_custom_logo' ) ) {
+          the_custom_logo();
+        } else {
+          bloginfo( 'name' );
+        }
+     ?>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-main" aria-controls="nav-main" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <?php
+    wp_nav_menu([
+      'menu'            => 'top',
+      'theme_location'  => 'top',
+      'container'       => 'div',
+      'container_id'    => 'nav-main',
+      'container_class' => 'collapse navbar-collapse',
+      'menu_id'         => false,
+      'menu_class'      => 'navbar-nav ml-auto mr-1',
+      'depth'           => 2,
+      'fallback_cb'     => 'bs4navwalker::fallback',
+      'walker'          => new bs4navwalker()
+    ]);
+    ?>
+  </div> 
+</nav>
