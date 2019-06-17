ff37b88bce10ac5a82db2a7110b96b5d267b9274
Rubén Segura <rsegrom@gmail.com>
2019-06-17 17:28:57 +0200

    dando formato a la página

--- single.php
+++ single.php
@@ -1,12 +1,29 @@
 <?php get_header() ?> 
 <div class="row"> 
-    <h1 class="text-center"><?php the_title() ?></h1>
+    <div class="col">
+        <h1><?php the_title() ?></h1>
+    </div>
 </div>
 <div class="row">
-    <p>Categorías: <?php the_category() ?></p><br>
-    <p>Etiquetas: <?php the_tags() ?></p>
+    <div class="col">
+        <p>Publicado el <?php the_time('j \d\e\ F \d\e\ Y \|\ H:i ') ?></p>
+        <p>Categorías: <?php the_category() ?></p>
+        <p>Etiquetado en: <?php the_tags() ?></p>
+    </div>
 </div>
 <div class="row">
-    <p><?php the_excerpt() ?></p>
+    <div class="col">
+        <p><?php the_excerpt() ?></p>
+    </div>
 </div>
+<div class="row">
+    <div class="col">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php the_content() ?>
+        <?php endwhile; else : ?>
+                <p><?php esc_html_e( 'No se ha encontrado ningún resultado.', 'rdv' ); ?></p>
+        <?php endif; ?>
+    </div>
+</div>
+
 <?php get_footer() ?>
