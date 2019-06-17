78ab2072d063176c39af9d64613e5ee20b45c39d
Rubén Segura <rsegrom@gmail.com>
2019-06-17 17:28:01 +0200

    añadida info a footer

--- footer.php
+++ footer.php
@@ -1,4 +1,11 @@
     <?php wp_footer() ?>
+    <footer>
+        <div class="row">
+            <div class="col">
+                <p class="text-center">Dont stop, code on! 👨🏻‍💻</p>
+            </div>
+        </div>
+    </footer>
     </div> <!-- main container end -->
 </body>
 </html>
