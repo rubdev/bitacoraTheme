f33352b3b6a2dbd33bf962b01d00f2bbf2c80ac7
Rubén Segura <rsegrom@gmail.com>
2019-06-18 18:41:26 +0200

    Añadidas descripción y maquetado con rows

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,22 +1,29 @@
 <nav class="navbar navbar-expand-md navbar-light bg-faded justify-content-end rdv-navbar">
   <div class="container">
-    <a id="rdv-texto-navbar"class="navbar-brand" href="<?php get_home_url( ) ?>"><?php bloginfo( 'name' )?></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-main" aria-controls="nav-main" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <?php
-    wp_nav_menu([
-      'menu'            => 'top',
-      'theme_location'  => 'top',
-      'container'       => 'div',
-      'container_id'    => 'nav-main',
-      'container_class' => 'collapse navbar-collapse',
-      'menu_id'         => false,
-      'menu_class'      => 'navbar-nav ml-auto mr-1',
-      'depth'           => 2,
-      'fallback_cb'     => 'bs4navwalker::fallback',
-      'walker'          => new bs4navwalker()
-    ]);
-    ?>
+    <div class="row">
+      <div class="col">
+      <a id="rdv-texto-navbar"class="navbar-brand" href="<?php get_home_url( ) ?>"><?php bloginfo( 'name' )?></a>
+      <p id="rdv-texto-navbar"><?php bloginfo( 'description' ) ?></p>
+      </div>
+      <div class="col">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-main" aria-controls="nav-main" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <?php
+        wp_nav_menu([
+          'menu'            => 'top',
+          'theme_location'  => 'top',
+          'container'       => 'div',
+          'container_id'    => 'nav-main',
+          'container_class' => 'collapse navbar-collapse',
+          'menu_id'         => false,
+          'menu_class'      => 'navbar-nav ml-auto mr-1',
+          'depth'           => 2,
+          'fallback_cb'     => 'bs4navwalker::fallback',
+          'walker'          => new bs4navwalker()
+        ]);
+        ?>
+      </div>
+    </div>
   </div> 
 </nav>
