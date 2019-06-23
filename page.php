08eb043050c2811ac51347c20da90852889719f0
Rubén Segura <rsegrom@gmail.com>
2019-06-23 23:11:39 +0200

    Estructura y estilos formulario

--- page.php
+++ page.php
@@ -1,11 +1,13 @@
 <?php get_header() ?> 
 <div class="row justify-content-center">
     <div class="col col-sm-8">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();       
-                    the_content(); 
-                endwhile; 
-            endif; 
-        ?>
+        <div class="rdv-page-style">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post();       
+                        the_content(); 
+                    endwhile; 
+                endif; 
+            ?>
+        </div>
     </div>
 </div>
 <?php get_footer() ?> 
