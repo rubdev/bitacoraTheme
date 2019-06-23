db9a28af4325f7d43cd9d124a70c763f8b2812e8
Rubén Segura <rsegrom@gmail.com>
2019-06-23 16:29:31 +0200

    Reglas css para post en pc

--- single.php
+++ single.php
@@ -1,28 +1,22 @@
 <?php get_header() ?> 
-<div class="row"> 
-    <div class="col">
-        <h1><?php the_title() ?></h1>
-    </div>
+<div class="row justify-content-center"> 
+    <h1 class="rdv-main-title"><?php the_title() ?></h1>
 </div>
-<div class="row">
-    <div class="col">
-        <p>Publicado el <?php the_time('j \d\e\ F \d\e\ Y \|\ H:i ') ?></p>
-        <p>Categorías: <?php the_category() ?></p>
-        <p>Etiquetado en: <?php the_tags() ?></p>
-    </div>
+<div id="rdv-info-post-top" class="row justify-content-center rdv-info-post">
+    <p>Publicado el <?php the_time('j \d\e\ F \d\e\ Y \|\ H:i ') ?></p>
 </div>
-<div class="row">
-    <div class="col">
-        <p><?php the_excerpt() ?></p>
-    </div>
+<div id="rdv-info-post-bottom" class="row justify-content-center rdv-info-post">
+    <p>Categorías: <?php echo get_the_category_list( ', ' ) ?></p>
 </div>
-<div class="row">
+<div class="row justify-content-center">
     <div class="col">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <?php the_content() ?>
-        <?php endwhile; else : ?>
-                <p><?php esc_html_e( 'No se ha encontrado ningún resultado.', 'rdv' ); ?></p>
-        <?php endif; ?>
+        <div class="rdv-contenido-post">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <?php the_content() ?>
+            <?php endwhile; else : ?>
+                    <p><?php esc_html_e( 'No se ha encontrado ningún resultado.', 'rdv' ); ?></p>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
 
